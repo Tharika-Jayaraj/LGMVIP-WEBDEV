7d2356111fda8038d44d7487ae0fcce42387b780
Tharika-Jayaraj <71089154+Tharika-Jayaraj@users.noreply.github.com>
2021-07-20 13:44:05 +0530

    Update App.js

--- Task2/App.js
+++ Task2/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Cards from "./Cards";
+import Cards from "./users";
 
 function App() {
   const [isDateLoaded, setIsDateLoaded] = React.useState(false);
